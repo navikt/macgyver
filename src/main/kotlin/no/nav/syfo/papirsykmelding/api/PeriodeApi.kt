a17b7987ec42e1bb1fff0af9aaa497b5d074b79d
Tia Firing <tia.firing@nav.no>
2022-12-01 07:19:49 +0100

    logger hvem som gjør endringen

--- src/main/kotlin/no/nav/syfo/papirsykmelding/api/PeriodeApi.kt
+++ src/main/kotlin/no/nav/syfo/papirsykmelding/api/PeriodeApi.kt
@@ -9,18 +9,23 @@ import io.ktor.server.routing.post
 import no.nav.syfo.application.HttpMessage
 import no.nav.syfo.log
 import no.nav.syfo.sykmelding.model.Periode
+import no.nav.syfo.utils.getAccessTokenFromAuthHeader
+import no.nav.syfo.utils.logNAVEpostAndActionToSecureLog
 
 fun Route.registrerPeriodeApi(updatePeriodeService: UpdatePeriodeService) {
     post("/api/papirsykmelding/{sykmeldingId}/periode") {
-        log.info("Mottatt kall for å oppdatere periode for sykmelding")
         val sykmeldingId = call.parameters["sykmeldingId"]!!
         if (sykmeldingId.isEmpty()) {
             call.respond(HttpStatusCode.BadRequest, HttpMessage("Sykmeldingid må være satt"))
             return@post
         }
+        log.info("Mottatt kall for å oppdatere periode for sykmelding med id $sykmeldingId")
 
-        log.info("Body: ${call.receive<String>()}")
-        
+        logNAVEpostAndActionToSecureLog(
+            getAccessTokenFromAuthHeader(call.request),
+            "endre perioder for sykmelding med id $sykmeldingId"
+        )
+
         val periodeListeDTO = call.receive<PeriodeListeDTO>()
         if (periodeListeDTO.periodeliste.isEmpty()) {
             call.respond(HttpStatusCode.BadRequest, HttpMessage("Periodelisten kan ikke være tom"))
