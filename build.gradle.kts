237db7dae6e70fdb772fc45e10d38ad48ab04ba8
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-01-11 10:30:17 +0100

    Upgrade kotlin
    Upgrade syfosm-common
    Cleanup in README.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ val javaxJaxwsApiVersion = "2.2.1"
 val jaxbApiVersion = "2.4.0-b180830.0359"
 val javaxActivationVersion = "1.1.1"
 val mockkVersion = "1.13.2"
-val smCommonVersion = "1.1490275"
+val smCommonVersion = "1.d6548c5"
 val sykmeldingVersion = "2019.07.29-02-53-86b22e73f7843e422ee500b486dac387a582f2d1"
 val fellesformatVersion = "2019.07.30-12-26-5c924ef4f04022bbb850aaf299eb8e4464c1ca6a"
 val kithHodemeldingVersion = "2019.07.30-12-26-5c924ef4f04022bbb850aaf299eb8e4464c1ca6a"
@@ -28,16 +28,15 @@ val javaTimeAdapterVersion = "1.1.3"
 val postgresVersion = "42.5.1"
 val confluentVersion = "6.2.2"
 val swaggerUiVersion = "4.15.0"
-val kotlinVersion = "1.7.22"
+val kotlinVersion = "1.8.0"
 val kotestVersion = "5.4.1"
 val googlePostgresVersion = "1.8.0"
 val junitVersion = "5.9.0"
 val nimbusdsVersion = "9.25.6"
-val nettyCodecVersion = "4.1.86.Final"
 
 
 plugins {
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "1.8.0"
     id("org.jmailen.kotlinter") version "3.12.0"
     id("com.diffplug.spotless") version "6.11.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -68,9 +67,6 @@ dependencies {
 
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    //This is to override version that is in io.ktor:ktor-server-netty
-    //https://www.cve.org/CVERecord?id=CVE-2022-41915
-    implementation("io.netty:netty-codec:$nettyCodecVersion")
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-server-auth:$ktorVersion")
     implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
@@ -133,6 +129,7 @@ swaggerSources {
 tasks {
     withType<Jar> {
         manifest.attributes["Main-Class"] = "no.nav.syfo.BootstrapKt"
+        dependsOn("generateSwaggerUI")
     }
 
     create("printVersion") {
@@ -142,6 +139,7 @@ tasks {
     }
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
+        dependsOn("generateSwaggerUI", "generateSwaggerUIMacgyver")
     }
 
     withType<org.hidetake.gradle.swagger.generator.GenerateSwaggerUI> {
