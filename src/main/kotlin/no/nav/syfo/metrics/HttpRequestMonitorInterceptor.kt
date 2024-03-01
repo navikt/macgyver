666230dbe7a197167e4ce022ef214286c8c853a4
Helene Arnesen <helene.arnesen@nav.no>
2024-03-01 16:57:52 +0100

    chore: add debug log

--- src/main/kotlin/no/nav/syfo/metrics/HttpRequestMonitorInterceptor.kt
+++ src/main/kotlin/no/nav/syfo/metrics/HttpRequestMonitorInterceptor.kt
@@ -4,10 +4,11 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.util.pipeline.PipelineContext
 import no.nav.syfo.logger
+import no.nav.syfo.sikkerlogg
 
 fun monitorHttpRequests(): suspend PipelineContext<Unit, ApplicationCall>.(Unit) -> Unit {
     return {
-        logger.info("Received request: ${call.request.uri}")
+        sikkerlogg.info("Received request: ${call.request.uri}")
         val label = context.request.path()
         val timer = HTTP_HISTOGRAM.labels(label).startTimer()
         proceed()
