9ba917a97faf908c5c6677a6729f18d78610aa0a
Andreas Sagen Aspaas <danduras@gmail.com>
2024-03-07 13:24:38 +0100

    chore: add year as query param
    Co-authored-by: Karl O <k@rl.run>
    Co-authored-by: Natalie Uranes <natalie.uranes@gmail.com>
    Co-authored-by: Joakim Taule Kartveit <joakimkartveit@gmail.com>
    Co-authored-by: Bendik Berntsen <Bendik.Berntsen@nav.no>
    Co-authored-by: Fridtjof Alestrøm <fridtjof.gustaf.alestrom@nav.no>

--- src/main/kotlin/no/nav/syfo/smregister/SmregisterApi.kt
+++ src/main/kotlin/no/nav/syfo/smregister/SmregisterApi.kt
@@ -4,45 +4,30 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.get
 import io.ktor.server.routing.post
-import jnr.a64asm.Offset
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import no.nav.syfo.db.Database
 import no.nav.syfo.db.toList
+import no.nav.syfo.kafka.aiven.KafkaUtils.Companion.getAivenKafkaConfig
+import no.nav.syfo.kafka.toProducerConfig
 import no.nav.syfo.logger
+import no.nav.syfo.utils.JacksonKafkaSerializer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
 import java.time.LocalDateTime
-import java.time.OffsetDateTime
+import java.util.Properties
 import kotlin.time.measureTimedValue
-
-fun Database.getSykmeldingIderAndMottattDato(): List<Pair<String, LocalDateTime>> {
-    var count: Long = 0
-    return connection.use {
-        it.prepareStatement(
-            """
-            select id, mottatt_tidspunkt from sykmeldingsopplysninger
-        """,
-        ).use { ps ->
-            ps.executeQuery().toList {
-                count++
-                if (count % 100_000 == 0L) {
-                    logger.info("Got $count sykmeldinger_ids from smregister")
-                }
-                getString("id") to getTimestamp("mottatt_tidspunkt").toLocalDateTime()
-            }
-        }
-    }
-}
-
+data class SykmeldingIdAndMottattDato(val sykmeldingId: String, val mottattDato: LocalDateTime)
 fun Database.getSykmeldingIderAndMottattDatoForYear(year: Int): List<Pair<String, LocalDateTime>> {
     var count: Long = 0
     return connection.use {
         it.prepareStatement(
             """
-            select count('any') from sykmeldingsopplysninger where extract(YEAR from mottatt_tidspunkt) = ?;
+            select id, mottatt_tidspunkt from sykmeldingsopplysninger where extract(YEAR from mottatt_tidspunkt) = ?;
         """,
         ).use { ps ->
             ps.setInt(1, year)
@@ -56,28 +41,46 @@ fun Database.getSykmeldingIderAndMottattDatoForYear(year: Int): List<Pair<String
         }
     }
 }
-
+val kafkaProducer = KafkaProducer<String, SykmeldingIdAndMottattDato>(
+    getAivenKafkaConfig("macgyver-migrations").toProducerConfig(
+        "macgyver-migrations",
+        JacksonKafkaSerializer::class,
+    )
+)
 @OptIn(DelicateCoroutinesApi::class)
 fun Route.setupSmregisterApi(database: Database) {
     var running = false
+    var error = false
     post("/api/smregister/job") {
-        val year = call.parameters.get("year")
-        if(running) {
+        val year = call.parameters.get("year")?.toIntOrNull()
+        if (year == null) {
+            call.respond(HttpStatusCode.BadRequest, mapOf("message" to "year is missing or not a number"))
+            return@post
+        }
+        if (running) {
             call.respond(HttpStatusCode.Conflict, mapOf("message" to "Job is already running"))
             return@post
         }
         call.respond(HttpStatusCode.Accepted, mapOf("running" to true))
         running = true
+        error = false
         GlobalScope.launch(Dispatchers.IO) {
             logger.info("Getting sykmeldinger_ids from smregister")
             try {
                 val timedValue = measureTimedValue {
-                    val yearNumber = year?.toIntOrNull()
-                    if(yearNumber != null) {
-                        logger.info("Getting sykmeldinger_ids from smregister for year $year")
-                         database.getSykmeldingIderAndMottattDatoForYear(yearNumber)
+                    logger.info("Getting sykmeldinger_ids from smregister for year $year")
+                    val result = database.getSykmeldingIderAndMottattDatoForYear(year).map {
+                        SykmeldingIdAndMottattDato(it.first, it.second)
                     }
-                    database.getSykmeldingIderAndMottattDato()
+                    result.forEach {
+                        kafkaProducer.send(ProducerRecord("tsm.regdump", it.sykmeldingId, it)) { metadata, exception ->
+                            if (exception != null && !error) {
+                                logger.error("Failed to send sykmelding_id to kafka", exception)
+                                error = true
+                            }
+                        }
+                    }
+                    result
                 }
                 logger.info("Got ${timedValue.value.size} sykmeldinger_ids from smregister for year $year in ${timedValue.duration.inWholeSeconds} seconds")
             } catch (e: Exception) {
