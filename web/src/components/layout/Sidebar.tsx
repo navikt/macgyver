aaa9a111b769966fc86ac46723b09d20f96af37c
Karl J. Overå <k@rl.run>
2024-05-29 09:38:45 +0200

    chore: add all features that needs to be moved over

--- web/src/components/layout/Sidebar.tsx
+++ web/src/components/layout/Sidebar.tsx
@@ -1,6 +1,12 @@
 import { ReactElement } from "react";
 import SidebarMenuItem from "./SidebarMenuItem.tsx";
-import { FilesIcon, PersonIcon } from "@navikt/aksel-icons";
+import {     BandageIcon,
+    FilesIcon,
+    FilePdfIcon,
+    ReceptionIcon,
+    PersonGroupIcon,
+    StethoscopeIcon,
+    PersonIcon, } from "@navikt/aksel-icons";
 import { Link } from "react-router-dom";
 
 type Props = {
@@ -10,9 +16,43 @@ type Props = {
 function Sidebar({ className }: Props): ReactElement {
   return (
     <div className={className}>
+      <SidebarMenuItem todo title="IdentEndring" Icon={PersonGroupIcon}>
+        <li>
+          <Link to="/ident-endring">Endre fnr for en gitt sykmelding</Link>
+        </li>
+      </SidebarMenuItem>
+      <SidebarMenuItem todo title="Sykmelding" Icon={BandageIcon}>
+        <li>
+          <Link to="/slett-sykmelding">Slett en gitt sykmelding</Link>
+        </li>
+      </SidebarMenuItem>
       <SidebarMenuItem title="Oppgave" Icon={FilesIcon}>
         <li>
           <Link to="/oppgave/oppslag">Hent oppgaver</Link>
+        </li>
+      </SidebarMenuItem>
+      <SidebarMenuItem todo title="Narmesteleder" Icon={ReceptionIcon}>
+        <>
+          <li>
+            <Link to="/ny-naermeste-leder">
+              Sender ny NL-request til altinn
+            </Link>
+          </li>
+          <li>
+            <Link to="/hent-naermeste-ledere">
+              Hent narmesteldere for ein sykmeldt person
+            </Link>
+          </li>
+        </>
+      </SidebarMenuItem>
+      <SidebarMenuItem todo title="Legeerklæring" Icon={StethoscopeIcon}>
+        <li>
+          <Link to="/slett-legeerklaering">Slett en gitt legeerklæring</Link>
+        </li>
+      </SidebarMenuItem>
+      <SidebarMenuItem todo title="Journalpost" Icon={FilePdfIcon}>
+        <li>
+          <Link to="/hent-journalposter">Hent liste med journalposter</Link>
         </li>
       </SidebarMenuItem>
       <SidebarMenuItem title="Person" Icon={PersonIcon}>
--- web/src/components/layout/SidebarMenuItem.tsx
+++ web/src/components/layout/SidebarMenuItem.tsx
@@ -1,24 +1,36 @@
-import { PropsWithChildren, ReactElement } from 'react'
+import { PropsWithChildren, ReactElement } from "react";
 
-import { Heading } from '@navikt/ds-react'
-import { PersonGroupIcon } from '@navikt/aksel-icons'
+import { Heading } from "@navikt/ds-react";
+import { PersonGroupIcon } from "@navikt/aksel-icons";
+import { cn } from "../../utils/tw";
 
 type Props = {
-    title: string
-    Icon: typeof PersonGroupIcon
-    children: ReactElement
+  title: string;
+  Icon: typeof PersonGroupIcon;
+  children: ReactElement;
+  // temp
+  todo?: true;
+};
+
+function SidebarMenuItem({
+  title,
+  Icon,
+  children,
+  todo,
+}: PropsWithChildren<Props>): ReactElement {
+  return (
+    <div
+      className={cn("pb-10", {
+        "opacity-50 pointer-events-none": todo,
+      })}
+    >
+      <Heading className="flex items-center pb-3" size="medium">
+        <Icon className="mr-2" />
+        {title}
+      </Heading>
+      <ul className="list-disc pl-12">{children}</ul>
+    </div>
+  );
 }
 
-function SidebarMenuItem({ title, Icon, children }: PropsWithChildren<Props>): ReactElement {
-    return (
-        <div className="pb-10">
-            <Heading className="flex items-center pb-3" size="medium">
-                <Icon className="mr-2" />
-                {title}
-            </Heading>
-            <ul className="list-disc pl-12">{children}</ul>
-        </div>
-    )
-}
-
-export default SidebarMenuItem
+export default SidebarMenuItem;
